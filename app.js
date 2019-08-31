83c1cf7dd522365a020aaab524dc15491fedb0fb
久松勇斗 <>
2019-08-31 15:20:31 +0900

    jQueryで日付の取得完成!

---
+++ app.js
@@ -0,0 +1,29 @@
+$(function(){
+
+    $cell = $('tbody').children('tr:first').siblings().children()
+    console.log( $cell );
+
+    $cell.each(function(index, element) {
+        $(element).on('click', function() {
+            let selectDate = $(element).text();
+            let selectMonth = $('#Month').text();
+            //キャッシュ　が問題　コマンド＋シフト＋R
+            let text = $('<div class="js"></div>').text(selectDate + 'th ' + selectMonth);
+            text.insertAfter('table');
+
+            console.log($('#selects > div:first'));
+            let setSelects = $('#selects > div:first').text(selectDate + 'th ' + selectMonth);
+            $('#selects').css('display', 'flex');
+            
+        });
+    });
+
+
+
+
+
+
+
+
+
+});
